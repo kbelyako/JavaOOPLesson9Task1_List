33f0a8f36a20fb3f944e22361f0f0509d1203d1e
Kir <kbelyako@yandex.com>
2019-07-28 17:40:10 +0600

    initial

---
+++ Lesson9Task1_List/src/com/yandex/kbelyako/Main.java
@@ -0,0 +1,27 @@
+package com.yandex.kbelyako;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Integer> testList = new ArrayList<Integer>();
+		Random rand = new Random();
+		 for (int i = 0; i < 10; i++) {
+			 
+			 int n = rand.nextInt(50);
+			 testList.add(n);
+
+			
+		}
+		
+
+		System.out.println(Arrays.toString(testList.toArray()));
+		testList.remove(0);
+		testList.remove(0);
+		testList.remove(testList.size()-1);
+		
+		System.out.println(Arrays.toString(testList.toArray()));
+		
+
+	}
+}
